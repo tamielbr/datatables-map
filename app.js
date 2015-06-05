08bccea33e890d62aeb2e5787bc6443d889c901c
tamielbr <tel@educacaoaberta.org>
2015-06-05 19:22:18 -0300

    now responsive and fixed search

--- app.js
+++ app.js
@@ -14,12 +14,14 @@ $(function() {
 
 // add search
 var searchLayer = function(query, cb) {
-var d = $.Deferred()
-// [{"loc":[41.57573,13.002411],"title":"black"},{"loc":[41.807149,13.162994],"title":"blue"}]
-//f.properies.XXXX, where XXXX defines what element search will be run on
-var json = data.features.map(function(f) {
+  var d = $.Deferred()
+
+  // [{"loc":[41.57573,13.002411],"title":"black"},{"loc":[41.807149,13.162994],"title":"blue"}
+  //f.properies.XXXX, where XXXX defines what element search will be run on (Nome, in this case)
+  var json = data.features.map(function(f) {
   return {loc: [f.geometry.coordinates[1], f.geometry.coordinates[0]], title: f.properties.Nome}
   })
+
   // return d.resolve(json)
   json = json.filter(function(result) {
     return RegExp(query.toLowerCase()).test(result.title.toLowerCase())
@@ -28,39 +30,36 @@ var json = data.features.map(function(f) {
     };
     map.addControl( new L.Control.Search({ callData: searchLayer }) );
 
-// gets data to display and automatically codes in geolocation for data display
-$.getJSON('json/geodata.json').then(function(geoJSON) {
-  var options = {
-    onEachFeature: onEachFeature
-    }
-    L.geoJson(geoJSON, options).addTo(map);
-    })
+    // gets data to display and automatically codes in geolocation for data display
+    var data;
+    $.getJSON('json/geodata.json').then(function(geoJSON) {
+      data = geoJSON;
+      var options = {
+        onEachFeature: onEachFeature
+        }
+        L.geoJson(geoJSON, options).addTo(map);
+        })
 
-// set sidebar
-var sidebar = L.control.sidebar('sidebar', {
-  position: 'right'
-  });
-map.addControl(sidebar);
-setTimeout(function () {
-  sidebar.hide();
-  }, 5000);
+    // set sidebar
+    var sidebar = L.control.sidebar('sidebar', {
+      position: 'right'
+      });
+      map.addControl(sidebar);
 
-//set sidebar content and functioning
-function onEachFeature (feature, layer) {
-    layer.on("click", function() {
-    $.when( $("#sidebar").load(feature.properties.file) ).done(function () {
-      sidebar.show();
-    })
-  });
+      //set sidebar content and hides about
+      //each pin clicked will call the related .html available in the json file (Arquivo)
+      function onEachFeature (feature, layer) {
+        layer.on("click", function() {
+          $("#sidebar").load(feature.properties.Arquivo)
+          $(".about").hide();
+          sidebar.show();
+}); //ends searchLayer
 
-  //for hiding the sidebar
+  //for hiding the sidebar and showing the "about" div again
   map.on("click",function() {
     sidebar.hide();
+    $(".about").show();
     })
 
-  //make the side bar disappear
-  $("#sidebar").on("dblclick",function(){
-    sidebar.hide();
-    })
   } //onEachFeature
 }); //main function
